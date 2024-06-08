bbb90332a801f36f0d2b92f515b60148ca781e88
unknown <7178497@clases.edu.sv>
2024-06-07 19:45:32 -0600

    se creeo el Proyecto

---
+++ PracticaEmpleado20240611/src/main/java/com/mycompany/practicaempleado20240611/PracticaEmpleado20240611.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicaempleado20240611;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class PracticaEmpleado20240611 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
